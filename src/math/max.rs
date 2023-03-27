647c0271c15f2b5253bf553aa6c15c927b151cde
Jd Chen <jd.chen.ed@gmail.com>
2023-03-27 23:32:41 +0800

    add max macro

---
+++ src/math/max.rs
@@ -0,0 +1,22 @@
+#[macro_export]
+macro_rules! max {
+    ($x:expr) => ($x);
+    ($x:expr, $($y:expr),+) => ($x.max(max!($($y),+)));
+    ($x:expr;) => ($x.iter().max());
+    ($x:expr; $s:expr, $e:expr) => ($x[$s..$e].iter().max());
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_number() {
+        assert_eq!(max!(1), 1);
+        assert_eq!(max!(1, 3, 2), 3);
+    }
+
+    #[test]
+    fn test_vec() {
+        assert_eq!(max!(vec![1, 3, 2, 8, 5];).unwrap(), &8);
+        assert_eq!(max!(vec![1, 3, 2]; 0, 3).unwrap(), &3);
+    }
+}
